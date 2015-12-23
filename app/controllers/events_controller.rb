04274d4bf050b30269482e7678728f833bbd0209
Cang Ta <hoksilato176@gmail.com>
2015-12-23 16:42:18 +0700

    finish search event at homepage

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all    
+    @search = params[:search].strip # Remove all leading and trailing whitespace
+
+    if @search.empty?
+      @events = Event.all
+    else
+      @events = Event.where('name LIKE ? OR extended_html_description LIKE ?', "%#{@search}%", "%#{@search}%").all
+    end
   end
 
   def show

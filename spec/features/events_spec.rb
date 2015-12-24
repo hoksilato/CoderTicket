0471016fde72ce42da78e79a26172227e7bf0140
Cang Ta <hoksilato176@gmail.com>
2015-12-24 16:57:54 +0700

    add feature tests: click link to view event detail and click button BOOK NOW to go to page for buying ticket

---
+++ spec/features/events_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Events' feature", type: :feature do
+  before do
+    @event = Event.create!(
+      starts_at: 1.day.ago,
+      extended_html_description: 'event have search string',
+      name: 'Event',
+      venue: Venue.create!(name: 'Venue'),
+      category: Category.create!(name: 'Entertainment')
+    )
+  end
+
+  scenario 'click on an event to see details about the event' do
+    visit root_path
+    visit event_path(@event)
+    expect(page).to have_selector('h3', text: 'Event')
+  end
+
+  scenario 'click on "Book Now" to go to a page to purchase tickets' do
+    visit event_path(@event)
+    click_link 'BOOK NOW'
+    expect(page).to have_selector 'div.tickets-buy'
+  end
+end
